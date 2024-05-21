886ead13e0cb963902f0622a6b9ff49b64472030
DarkFlo <fhock2002@gmail.com>
2024-05-21 20:17:54 +0200

    created destroy_gpu_timetable

--- include/nigiri/routing/gpu.h
+++ include/nigiri/routing/gpu.h
@@ -6,14 +6,15 @@
 
 extern "C" {
 
-struct gpu_timetable;
+  struct gpu_timetable;
 
-struct gpu_delta {
-  std::uint16_t days_ : 5;
-  std::uint16_t mam_ : 11;
-};
+  struct gpu_delta {
+    std::uint16_t days_ : 5;
+    std::uint16_t mam_ : 11;
+  };
 
-struct gpu_timetable* create_gpu_timetable(gpu_delta const* route_stop_times,
+  struct gpu_timetable* create_gpu_timetable(gpu_delta const* route_stop_times,
                                            std::uint32_t n_route_stop_times);
+  bool destroy_gpu_timetable(gpu_timetable* gtt);
 
 }  // extern "C"
