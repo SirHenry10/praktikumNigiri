0eae7444166d47d59f470a92c273b7cf625313b3
nikedratt <nikedratt@gmail.com>
2024-05-29 18:31:25 +0200

    TODO GPU Timetable Attribute 14/15

--- include/nigiri/routing/gpu.h
+++ include/nigiri/routing/gpu.h
@@ -16,10 +16,41 @@ extern "C" {
   };
 
   struct gpu_timetable* create_gpu_timetable(gpu_delta const* route_stop_times,
-                                           std::uint32_t n_route_stop_times,
-                                           nigiri::route_idx_t* route_stop_time_ranges_keys_keys,
-                                           nigiri::interval<std::uint32_t>* route_stop_time_ranges_values,
-                                           std::uint32_t n_route_stop_time_ranges_);
+                                             std::uint32_t n_route_stop_times,
+                                             nigiri::route_idx_t* location_routes,
+                                             std::uint32_t n_locations,
+                                             nigiri::route_idx_t* route_clasz_keys,
+                                             nigiri::clasz* route_clasz_values,
+                                             std::uint32_t n_route_clasz,
+                                             nigiri::location_idx_t* transfer_time_keys,
+                                             nigiri::u8_minutes* transfer_time_values,
+                                             std::uint32_t n_transfer_time,
+                                             nigiri::footpath* footpaths_out,
+                                             std::uint32_t n_footpaths_out,
+                                             stop::value_type* route_location_seq,
+                                             std::uint32_t n_route_location_seq,
+                                             nigiri::route_idx_t* route_transport_ranges_keys,
+                                             nigiri::interval<transport_idx_t>* route_transport_ranges_values,
+                                             std::uint32_t n_route_transport_ranges,
+                                             nigiri::bitfield_idx_t* bitfields_keys,
+                                             nigiri::bitfield* bitfields_values,
+                                             std::uint32_t n_bitfields,
+                                             nigiri::transport_idx_t* transport_traffic_days_keys,
+                                             nigiri::bitfield_idx_t* transport_traffic_days_values,
+                                             std::uint32_t n_transport_traffic_days,
+                                             interval<date::sys_days> date_range, //ich glaube durch dieses intervall müssen wir nicht iterieren bzw. dazu sind intervalle gar nicht gedacht
+                                             char* trip_display_names,
+                                             std::uint32_t n_trip_display_names,
+                                             nigiri::trip_idx_t* merged_trips,
+                                             std::uint32_t n_merged_trips,
+                                             nigiri::merged_trips_idx_t* transport_to_trip_section,
+                                             std::uint32_t n_transport_to_trip_section,
+                                             nigiri::transport_idx_t* transport_route_keys,
+                                             nigiri::route_idx_t* transport_route_values,
+                                             std::uint32_t n_transport_routes,
+                                             nigiri::route_idx_t* route_stop_time_ranges_keys_keys,
+                                             nigiri::interval<std::uint32_t>* route_stop_time_ranges_values,
+                                             std::uint32_t n_route_stop_time_ranges_);
   void destroy_gpu_timetable(gpu_timetable* &gtt);
 
 }  // extern "C"
--- src/routing/gpu.cu
+++ src/routing/gpu.cu
@@ -26,10 +26,61 @@ extern "C" {
     gpu_delta* route_stop_times_{nullptr};
     nigiri::route_idx_t* route_stop_time_ranges_keys {nullptr};
     nigiri::interval<std::uint32_t>* route_stop_time_ranges_values {nullptr};
+    nigiri::route_idx_t* location_routes_{nullptr};
+    nigiri::route_idx_t* route_clasz_keys_{nullptr};
+    nigiri::clasz* route_clasz_values_{nullptr};
+    nigiri::location_idx_t* transfer_time_keys_{nullptr};
+    nigiri::u8_minutes* transfer_time_values_{nullptr};
+    nigiri::footpath* footpaths_out_{nullptr};
+    stop::value_type* route_location_seq_{nullptr};
+    nigiri::route_idx_t* route_transport_ranges_keys_{nullptr};
+    nigiri::interval<transport_idx_t>* route_transport_ranges_values_{nullptr};
+    nigiri::bitfield_idx_t* bitfields_keys_{nullptr};
+    nigiri::bitfield* bitfields_values_{nullptr};
+    nigiri::transport_idx_t* transport_traffic_days_keys_{nullptr};
+    nigiri::bitfield_idx_t* transport_traffic_days_values_{nullptr};
+    char* trip_display_names_{nullptr};
+    nigiri::trip_idx_t* merged_trips_{nullptr};
+    nigiri::merged_trips_idx_t* transport_to_trip_section_{nullptr};
+    nigiri::transport_idx_t* transport_route_keys_{nullptr};
+    nigiri::route_idx_t* transport_route_values_{nullptr};
+    nigiri::route_idx_t* route_stop_time_ranges_keys_keys_{nullptr};
+    nigiri::interval<std::uint32_t>* route_stop_time_ranges_values_{nullptr};
   };
 
   struct gpu_timetable* create_gpu_timetable(gpu_delta const* route_stop_times,
                                              std::uint32_t n_route_stop_times,
+                                             nigiri::route_idx_t* location_routes,
+                                             std::uint32_t n_locations,
+                                             nigiri::route_idx_t* route_clasz_keys,
+                                             nigiri::clasz* route_clasz_values,
+                                             std::uint32_t n_route_clasz,
+                                             nigiri::location_idx_t* transfer_time_keys,
+                                             nigiri::u8_minutes* transfer_time_values,
+                                             std::uint32_t n_transfer_time,
+                                             nigiri::footpath* footpaths_out,
+                                             std::uint32_t n_footpaths_out,
+                                             stop::value_type* route_location_seq,
+                                             std::uint32_t n_route_location_seq,
+                                             nigiri::route_idx_t* route_transport_ranges_keys,
+                                             nigiri::interval<transport_idx_t>* route_transport_ranges_values,
+                                             std::uint32_t n_route_transport_ranges,
+                                             nigiri::bitfield_idx_t* bitfields_keys,
+                                             nigiri::bitfield* bitfields_values,
+                                             std::uint32_t n_bitfields,
+                                             nigiri::transport_idx_t* transport_traffic_days_keys,
+                                             nigiri::bitfield_idx_t* transport_traffic_days_values,
+                                             std::uint32_t n_transport_traffic_days,
+                                             interval<date::sys_days> date_range,
+                                             char* trip_display_names,
+                                             std::uint32_t n_trip_display_names,
+                                             nigiri::trip_idx_t* merged_trips,
+                                             std::uint32_t n_merged_trips,
+                                             nigiri::merged_trips_idx_t* transport_to_trip_section,
+                                             std::uint32_t n_transport_to_trip_section,
+                                             nigiri::transport_idx_t* transport_route_keys,
+                                             nigiri::route_idx_t* transport_route_values,
+                                             std::uint32_t n_transport_routes,
                                              nigiri::route_idx_t* route_stop_time_ranges_keys_keys,
                                              nigiri::interval<std::uint32_t>* route_stop_time_ranges_values,
                                              std::uint32_t n_route_stop_time_ranges_) {
@@ -55,30 +106,55 @@ extern "C" {
     CUDA_COPY_TO_DEVICE(nigiri::interval<std::uint32_t>, gtt->route_stop_time_ranges_values, route_stop_time_ranges_values,
                         n_route_stop_time_ranges_);
     //location_routes_
-
+    gtt->location_routes_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::_route_idx_t, gtt->location_routes_, location_routes, n_locations);
     //route_clasz
-
-    //location_transfer_time_count
-
+    gtt->route_clasz_keys_ = nullptr;
+    gtt->route_clasz_values_= nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::route_idx_t, gtt->route_clasz_keys_, route_clasz_keys, n_route_clasz);
+    CUDA_COPY_TO_DEVICE(nigiri::clasz, gtt->route_clasz_values_, route_clasz_values, n_route_clasz);
+    //transfer_time_
+    gtt->transfer_time_keys_ = nullptr;
+    gtt->transfer_time_values_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::location_idx_t, gtt->transfer_time_keys_, transfer_time_keys, n_transfer_time);
+    CUDA_COPY_TO_DEVICE(nigiri::u8_minutes, gtt->transfer_time_values_, transfer_time_values, n_transfer_time);
     //locations_footpaths_out
-
+    gtt->footpaths_out_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::footpath, gtt->footpaths_out_, footpaths_out, n_footpaths_out);
     //route_location_seq
-
+    gtt->route_location_seq_ = nullptr;
+    CUDA_COPY_TO_DEVICE(stop::value_type, gtt->route_location_seq_, route_location_seq, n_route_location_seq);
     //route_transport_ranges
-
+    gtt->route_transport_ranges_keys_ = nullptr;
+    gtt->route_transport_ranges_values_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::route_idx_t, gtt->route_transport_ranges_keys_, route_transport_ranges_keys, n_route_transport_ranges);
+    CUDA_COPY_TO_DEVICE(nigiri::interval<transport_idx_t>, gtt->route_transport_ranges_values_, route_transport_ranges_values, n_route_transport_ranges);
     //bitfields_
-
+    gtt->bitfields_keys_ = nullptr;
+    gtt->bitfields_values_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::bitfield_idx_t, gtt->bitfields_keys_, bitfields_keys, n_bitfields);
+    CUDA_COPY_TO_DEVICE(nigiri::bitfield, gtt->bitfields_values_, bitfields_values, n_bitfields);
     //transport_traffic_days_
-
+    gtt->transport_traffic_days_keys_ = nullptr;
+    gtt->transport_traffic_days_values_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::transport_idx_t, gtt->transport_traffic_days_keys_, transport_traffic_days_keys, n_transport_traffic_days);
+    CUDA_COPY_TO_DEVICE(nigiri::bitfield_idx_t, gtt->transport_traffic_days_values_, transport_traffic_days_values, n_transport_traffic_days);
     //date_range_
 
     //trip_display_names_
-
+    gtt->trip_display_names_ = nullptr;
+    CUDA_COPY_TO_DEVICE(char, gtt->trip_display_names_, trip_display_names, n_trip_display_names);
     //merged_trips_
-
+    gtt->merged_trips_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::trip_idx_t, gtt->merged_trips_, merged_trips, n_merged_trips);
     //transport_to_trip_section_
-
+    gtt->transport_to_trip_section_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::merged_trips_idx_t, gtt->transport_to_trip_section_, transport_to_trip_section, n_transport_to_trip_section);
     //transport_route_
+    gtt->transport_route_keys_ = nullptr;
+    gtt->transport_route_values_ = nullptr;
+    CUDA_COPY_TO_DEVICE(nigiri::transport_idx_t, gtt->transport_route_keys_, transport_route_keys, n_transport_routes);
+    CUDA_COPY_TO_DEVICE(nigiri::route_idx_t, gtt->transport_route_values_, transport_route_values, n_transport_routes);
 
     return gtt;
 
@@ -89,30 +165,38 @@ extern "C" {
     cudaFree(gtt->route_stop_time_ranges_keys);
     cudaFree(gtt->route_stop_time_ranges_values);
     //location_routes_
-
+    cudaFree(gtt->location_routes_);
     //route_clasz
-
+    cudaFree(gtt->route_clasz_keys_);
+    cudaFree(gtt->route_clasz_values_);
     //location_transfer_time_count
-
+    cudaFree(gtt->transfer_time_keys_);
+    cudaFree(gtt->transfer_time_values_);
     //locations_footpaths_out
-
+    cudaFree(gtt->footpaths_out_);
     //route_location_seq
-
+    cudaFree(gtt->route_location_seq_);
     //route_transport_ranges
-
+    cudaFree(gtt->route_transport_ranges_keys_);
+    cudafree(gtt->route_transport_ranges_values_);
     //bitfields_
-
+    cudaFree(gtt->bitfields_keys_);
+    cudaFree(gtt->bitfields_values_);
     //transport_traffic_days_
-
+    cudaFree(gtt->transport_traffic_days_keys_);
+    cudaFree(gtt->transport_traffic_days_values_);
     //date_range_
 
     //trip_display_names_
-
+    cudaFree(gtt->trip_display_names_);
     //merged_trips_
-
+    cudaFree(gtt->merged_trips_);
     //transport_to_trip_section_
-
+    cudaFree(gtt->transport_to_trip_section_);
     //transport_route_
+    cudaFree(gtt->transport_route_keys_);
+    cudaFree(gtt->transport_route_values_);
+
     free(gtt);
     return nullptr;
   }
@@ -123,30 +207,38 @@ extern "C" {
       cudaFree(gtt->route_stop_time_ranges_keys);
       cudaFree(gtt->route_stop_time_ranges_values);
       //location_routes_
-
+      cudaFree(gtt->location_routes_);
       //route_clasz
-
+      cudaFree(gtt->route_clasz_keys_);
+      cudaFree(gtt->route_clasz_values_);
       //location_transfer_time_count
-
+      cudaFree(gtt->transfer_time_keys_);
+      cudaFree(gtt->transfer_time_values_);
       //locations_footpaths_out
-
+      cudaFree(gtt->footpaths_out_);
       //route_location_seq
-
+      cudaFree(gtt->route_location_seq_);
       //route_transport_ranges
-
+      cudaFree(gtt->route_transport_ranges_keys_);
+      cudafree(gtt->route_transport_ranges_values_);
       //bitfields_
-
+      cudaFree(gtt->bitfields_keys_);
+      cudaFree(gtt->bitfields_values_);
       //transport_traffic_days_
-
+      cudaFree(gtt->transport_traffic_days_keys_);
+      cudaFree(gtt->transport_traffic_days_values_);
       //date_range_
 
       //trip_display_names_
-
+      cudaFree(gtt->trip_display_names_);
       //merged_trips_
-
+      cudaFree(gtt->merged_trips_);
       //transport_to_trip_section_
-
+      cudaFree(gtt->transport_to_trip_section_);
       //transport_route_
+      cudaFree(gtt->transport_route_keys_);
+      cudaFree(gtt->transport_route_values_);
+
       free(gtt);
       gtt = nullptr;
       cudaDeviceSynchronize();
